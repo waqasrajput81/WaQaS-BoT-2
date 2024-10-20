12fb49ca9f40a613f40c103d02f5ce45ab54c993
Zainiprince7 <fazooole@gmail.com>
2024-10-20 20:03:56 +0500

    Update imgur.js

--- Priyansh/commands/imgure.js
+++ Priyansh/commands/imgure.js
@@ -1,22 +1,54 @@
 module.exports.config = {
-    name: "imgur",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-    description: "",
-    commandCategory: "Game",
-    usages: "[reply]",
-    cooldowns: 5,
-    dependencies: {
-      "axios": ""
-    }
+  name: "imgur",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Nayan",
+  description: "",
+  prefix: true,
+  category: "user",
+  usages: "Link",
+  cooldowns: 5,
+  dependencies: {
+    "axios": "",
+    "imgur-upload-api": ""
+  }
 };
 
-module.exports.run = async ({ api, event }) => {
-const axios = global.nodemodule['axios'];  
-var linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    if(!linkanh) return api.sendMessage('Please reply or enter a link 1 image!!!', event.threadID, event.messageID)
-const res = await axios.get(`https://imgur-api-by-koja.xx0xkoja.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);    
-var img = res.data.uploaded.image;
-    return api.sendMessage(`${img}`, event.threadID, event.messageID);
-}
+module.exports.run = async ({ api, event, args }) => {
+  const axios = global.nodemodule['axios'];
+  const { imgur } = require("imgur-upload-api");
+
+
+  let linkanh = event.messageReply?.attachments[0]?.url || args.join(" ");
+
+  if (!linkanh) {
+    return api.sendMessage('[⚜️]➜ Please provide an image or video link.', event.threadID, event.messageID);
+  }
+
+  try {
+    
+    linkanh = linkanh.replace(/\s/g, '');
+
+    
+    if (!/^https?:\/\//.test(linkanh)) {
+      return api.sendMessage('[⚜️]➜ Invalid URL: URL must start with http:// or https://', event.threadID, event.messageID);
+    }
+
+    
+    const encodedUrl = encodeURI(linkanh);
+
+    const attachments = event.messageReply?.attachments || [];
+    const allPromises = attachments.map(item => {
+      const encodedItemUrl = encodeURI(item.url);
+      return imgur(encodedItemUrl);
+    });
+
+    const results = await Promise.all(allPromises);
+    const imgurLinks = results.map(result => result.data.link); 
+
+    return api.sendMessage(`Uploaded Imgur Links:\n${imgurLinks.join('\n')}`, event.threadID, event.messageID);
+  } catch (e) {
+    console.error(e);
+    return api.sendMessage('[⚜️]➜ An error occurred while uploading the image or video.', event.threadID, event.messageID);
+  }
+};
