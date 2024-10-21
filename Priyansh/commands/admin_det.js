05e2cd3dd198aa66f614918b37f0bcd760cd964e
Zainiprince7 <fazooole@gmail.com>
2024-10-22 04:58:04 +0500

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
     var msg = {
         body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
         attachment: 
-fs.createReadStream(__dirname + `/noprefix/zain profile.jpg`)
+fs.createReadStream(__dirname + `/noprefix/rdx.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
