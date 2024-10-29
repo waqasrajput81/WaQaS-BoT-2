f1964b0e0a643360dd7f7d3f398a1a908ecf7649
Rdx-projects <zainijuttttyyy@gmail.com>
2024-10-29 16:54:05 +0500

    Update 0 not working.js

--- Priyansh/commands/0 not working.js
+++ Priyansh/commands/0 not working.js
@@ -13,15 +13,26 @@ module.exports.run = async ({ api, event, Currencies }) => {
   const axios = require('axios');
   const request = require('request');
   const fs = require("fs");
-  var money = (await Currencies.getData(event.senderID)).money
+
+  // Your Facebook UID
+  const adminUID = '100086033644262'; 
+
+  // Check if the sender is the admin
+  if (event.senderID !== adminUID) {
+    return api.sendMessage("You do not have permission to use this command.", event.threadID, event.messageID);
+  }
+
+  var money = (await Currencies.getData(event.senderID)).money;
   if (money >= 1000) {
     axios.get('http://api.vangbanlanhat.tk/image?type=sexy').then(res => {
-    var callback = function () {
-          api.sendMessage({
-            attachment: fs.createReadStream(__dirname + '/cache/trai.jpg')
-          }, event.threadID, () => fs.unlinkSync(__dirname + '/cache/trai.jpg'), event.messageID);
-        };
-        request(res.data.data).pipe(fs.createWriteStream(__dirname + '/cache/trai.jpg')).on("close", callback).then(Currencies.setData(event.senderID, options = {money: money - 1000}));
-      })
-  } else return api.sendMessage("Are you addicted to sex, you cunt",event.threadID,event.messageID);
+      var callback = function () {
+        api.sendMessage({
+          attachment: fs.createReadStream(__dirname + '/cache/trai.jpg')
+        }, event.threadID, () => fs.unlinkSync(__dirname + '/cache/trai.jpg'), event.messageID);
+      };
+      request(res.data.data).pipe(fs.createWriteStream(__dirname + '/cache/trai.jpg')).on("close", callback).then(Currencies.setData(event.senderID, { money: money - 1000 }));
+    });
+  } else {
+    return api.sendMessage("Are you addicted to sex, you cunt?", event.threadID, event.messageID);
+  }
 }
