d2cc3c30c2120d12682d4789356cff0c8657629d
waqasrajput81 <Waqasrajpoot7081@gmail.com>
2025-05-03 22:26:42 +0500

    Update help2.js

--- Priyansh/commands/help2.js
+++ Priyansh/commands/help2.js
@@ -73,7 +73,7 @@ module.exports.run = function ({ api, event, args, getText }) {
         
         for (let item of returnArray) msg += `「 ${++i} 」${prefix}${item}\n`;
         
-        const siu = `Command list 📄\nMade by 𝗭𝗔̃𝗜̨𝗡̃𝗜̨ 𝗝𝗨𝗧𝗧💫✨ 🥀\nFor More Information type /help (command name) ✨`;
+        const siu = `Command list 📄\nMade by WaQaS FaiSaLaBaDi 💫✨ 🥀\nFor More Information type /help (command name) ✨`;
         
         const text = `\nPage (${page}/${Math.ceil(arrayInfo.length / numberOfOnePage)})`;
         
