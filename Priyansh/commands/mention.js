69647883b3220f8a4b09a5792a2a8120e57bf4ac
waqasrajput81 <Waqasrajpoot7081@gmail.com>
2025-05-05 01:19:17 +0500

    Update mention.js

--- Priyansh/commands/mention.js
+++ Priyansh/commands/mention.js
@@ -9,8 +9,8 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100086033644262") {
-    var aid = ["100086033644262"];
+  if (event.senderID !== "61571580477330") {
+    var aid = ["61571580477330"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "WaQas Toh Chala gaya"];
