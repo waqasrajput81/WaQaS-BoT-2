fd0ea148b623d1af3630fdb70ffb0f9e41f51b0d
waqasrajput81 <Waqasrajpoot7081@gmail.com>
2025-05-05 11:23:59 +0500

    Update ai-siri.js

--- Priyansh/commands/ai-siri.js
+++ Priyansh/commands/ai-siri.js
@@ -13,7 +13,7 @@ const config = {
 
 const handleEvent = async function ({ api, event, client, __GLOBAL }) {
 
-  if (event.body.indexOf("siri") === 0 || event.body.indexOf("Siri") === 0 || event.body.indexOf("WaQaS") === 0 || event.body.indexOf("Zain") === 0)  {
+  if (event.body.indexOf("siri") === 0 || event.body.indexOf("Siri") === 0 || event.body.indexOf("WaQaS") === 0 || event.body.indexOf("WaQaS") === 0)  {
     const { threadID, messageID } = event;
     const input = event.body;
     const message = input.split(" ");
