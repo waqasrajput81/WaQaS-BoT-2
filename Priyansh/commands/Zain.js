a70029abd1d7bb843644083188a855df6d7fdb56
waqasrajput81 <Waqasrajpoot7081@gmail.com>
2025-05-06 09:56:28 +0500

    Update zain.js

--- Priyansh/commands/Zain.js
+++ Priyansh/commands/Zain.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("arun")) {
 		var msg = {
 				body: "★꧁💗𝕄𝕐 ℍ𝔸ℕ𝔻𝕊𝕆𝕄𝔼 𝕆𝕎ℕ𝔼ℝ🥰꧂                    𝗠𝗮𝗱𝗲 𝗕𝘆🫶★\n\n✦꧁WaQaS-FaiSaLaBaDi꧂✦\n\n★★᭄WaQaS 𝐤𝐚 𝐛𝐨𝐭 𝐡𝐮 𝐦𝐚𝐬𝐨𝐨𝐦 𝐬𝐚 🥺:\n\n✦ 𝘄𝗮𝘀𝗲 𝗼𝘄𝗻𝗲𝗿 𝗴 𝘃𝗶𝗱𝗲𝗼 𝗯𝗿𝗶 𝗹𝗴𝗮𝗲𝘆🙈🔥  ✦ \n𝗡𝗢𝗧𝗘 \n 𝙱𝙾𝚃 𝙼𝙰 𝙺𝙸𝚂𝙸 𝙱𝙷𝙸 𝙺𝙸𝚂𝙼 𝙺𝙰 𝙸𝚂𝚂𝚄𝙴 𝚈𝙰 𝙰𝙿𝙽𝙴 𝙽𝙰𝙼𝙴 𝙺𝙰 𝙱𝙾𝚃 𝙱𝙽𝚆𝙰𝙽𝙴 𝙺 𝙻𝚈 𝙽𝙴𝙲𝙷𝚈 𝙳𝚈𝙴 𝙶𝚈𝙴 𝙻𝙸𝙽𝙺 𝙿𝙰 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙺𝚁𝚈👇👇  😋https://www.facebook.com/sardarjutt.jutt.92?mibextid=kFxxJD`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/2211.jpg`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/vicky.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
 		api.setMessageReaction("🫡", event.messageID, (err) => {}, true)
